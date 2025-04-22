892a96a4380bdcee2c37efeaafc0b21e57cebffa
Roderick Muñoz <roderick.munozg@gmail.com>
2025-04-21 23:20:39 -0400

    toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar"; 
 import Footer from "./components/Footer";
@@ -20,8 +22,6 @@ import AdminServicioForm from "./pages/admin/AdminServicioForm";
 import AdminReservaDetalle from "./pages/admin/AdminReservaDetalle";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
